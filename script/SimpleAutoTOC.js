abef6a42339722929eef6ec73c0e484803210c7b
Leejun Liao <liaoleejun@163.com>
2019-01-29 22:19:50 +0800

    Initial commit

---
+++ script/SimpleAutoTOC.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#toc").append('<p><b>Table of Content</b></p>');
+    $("h1, h2, h3").each(function(i) {
+        let current = $(this);
+        current.attr("id", "title" + i);
+
+        $("#toc").append("<a id='link" + i + "' href='#title" + i + "' title='" + current.prop("tagName") + "'>" + current.html() + "</a>");
+    });
+});
